b90ee37fb5c409d41e266df97b2f32ef74879f00
PLATA <abhiketan1@gmail.com>
2024-09-09 19:50:33 -0400

    Check out what this is later

--- backend/tempCodeRunnerFile.py
+++ backend/tempCodeRunnerFile.py
@@ -2,4 +2,3 @@
     #                                             'phylogenetic_tree.png')
     # phylogenetic_tree_pipeline(filepath, phylogenetic_tree_image_path)
     # results['phylogenetic_tree'] = phylogenetic_tree_image_path
-
